86669ec255afa350b5e7439041ae0c3cde8263c1
kalash <kalashvasaniya@gmail.com>
2022-09-25 23:17:31 +0530

    change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.set("view engine", "ejs");
+app.use(express.static ("public"));
 app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("index");
