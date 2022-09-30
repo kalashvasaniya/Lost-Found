52fbd065f2f6e8c60cad4e395825459d9a7b2a24
Dharansh Neema <dharanshneema@gmail.com>
2022-09-30 18:20:20 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/LostAndFound");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const dataBase = require("./modules/userSchema");
+
 const path = require("path");
 app.set("view engine", "ejs");
-app.use(express.static ("public"));
+app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("index");
