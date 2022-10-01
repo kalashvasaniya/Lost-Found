4a5d61ace7020492ce16dbfd2e5bfdafa366dffb
kalash <kalashvasaniya@gmail.com>
2022-10-02 04:06:12 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
       colors: {
         'body': '#17171F',
         'selected-text': '#A3A3FF',
-        'theme': '#3F3FFF',
         'nav': '#404053',
         'secondary': '#9191A4',
         'badge': '#3F3F51',
